fa052047d0980b6ce1899fdfa5c4ab580b4c41cf
Mohiuddin_Murad <104640147+muradmy00@users.noreply.github.com>
2024-05-01 23:08:50 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// function showContent(category) {
+//   var contentDiv = document.getElementById('content');
+//   switch(category) {
+//     case 'Live TV':
+//       contentDiv.innerHTML = "<h2>Live TV</h2><p>Live TV content goes here.</p>";
+//       break;
+//     case 'Sports':
+//       contentDiv.innerHTML = "<h2>Football</h2><p>Link1</p>";
+//       contentDiv.innerHTML = "<h2>Cricket</h2><a href='https://alkora.kora-goal.online/p/as-goal-as-goals.html' target='_blank'>Link1</a>";
+//       contentDiv.innerHTML = "<a href='https://alkora.kora-goal.online/p/as-goal-as-goals.html' target='_blank'>Link1</a>";
+//       break;
+//     case 'Movies':
+//       contentDiv.innerHTML = "<h2>Movies</h2><p>Movies content goes here.</p>";
+//       contentDiv.innerHTML = "<h2>Live TV</h2><p>Please</p>";
+//       break;
+//     case 'News':
+//       contentDiv.innerHTML = "<h2>News</h2><p>News content goes here.</p>";
+//       break;
+//     case 'Entertainment':
+//       contentDiv.innerHTML = "<h2>Entertainment</h2><p>Entertainment content goes here.</p>";
+//       break;
+//     default:
+//       contentDiv.innerHTML = "<h2>Welcome to Live TV</h2><p>Select a category from the navigation menu above.</p>";
+//   }
+// }
